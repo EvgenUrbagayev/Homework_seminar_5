05b1156869c6a8f113b11655c3245fbb3d672e05
Evgeniy <eurbagayev@mail.com>
2022-10-08 12:30:30 +0300

    Разница между max и min элементами массива

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу
+// между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+using static System.Console;
+Clear();
+WriteLine("Введите размер массива:  ");
+int lenght = int.Parse(ReadLine()!);
+WriteLine("Введите минимальное значение массива:  ");
+int min = int.Parse(ReadLine()!);
+WriteLine("Введите максимальное значение:  ");
+int max = int.Parse(ReadLine()!);
+
+
+int[] array = GetArray(lenght, min, max);
+
+int minElement = array[0];
+
+for(int i = 1; i < array.Length; i += 1)
+{
+    if(minElement > array[i])
+    {
+        minElement = array[i];
+    }
+}
+ WriteLine($"Минимальный элемент массива: {minElement}");
+ 
+ 
+int maxElement = array[0];
+for(int i = 1; i < array.Length; i += 1)
+{
+    if(array[i] > maxElement)
+    {
+       maxElement = array[i];
+    }
+}
+WriteLine($"Максимальный элемент массива: {maxElement}");
+
+
+WriteLine($"Разница между максимальным и минимальным элементами массива равна {maxElement - minElement}");
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+
+{
+    Random rnd = new Random();
+    int[] result = new int[size];
+    for(int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue+1);
+        Write($"{result[i]}, ");
+    }
+   
+    return result;
+    
+}
